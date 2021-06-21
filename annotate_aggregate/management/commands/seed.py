34b9a7b40184fa8706141cb08e011a8d97714312
Serhii Voitko <voitko.serhii@gmail.com>
2021-06-21 16:04:53 +0300

    jst

--- annotate_aggregate/management/commands/seed.py
+++ annotate_aggregate/management/commands/seed.py
@@ -1,41 +1,38 @@
 import random
 from django.utils import timezone
 from annotate_aggregate.models import Author, Book, Store, Publisher
-from django.db.models import Q
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
 from faker import Faker
 
 fake = Faker()
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('arg', type=int)
 
     def handle(self, *args, **options):
-        authors = [Author(name=fake.name(), age=random.randint(20, 70)) for _ in range(1, 6)]
-        Author.objects.bulk_create(authors)
+        num = options['arg']
 
-        # create 5 publishers
+        for _ in range(int(num)):
+            Author.objects.create(name=fake.name(), age=random.randint(20, 70))
 
-        publishers = [Publisher(name=fake.company()) for _ in range(1, 6)]
-        Publisher.objects.bulk_create(publishers)
-        # create 20 books for every publishers
+        for _ in range(int(num)):
+            Publisher.objects.create(name=fake.company())
 
-        for publisher in Publisher.objects.all():
-            for i in range(1, 6):
-                books = [Book(
-                    name=fake.sentence(),
-                    price=random.uniform(29.99, 225.9),
-                    pages=random.randint(50, 300),
-                    rating=round(random.uniform(1, 5), 2),
-                    pubdate=timezone.now(),
-                    publisher=publisher)]
-                Book.objects.bulk_create(books)
-        # create 10 stores and insert 10 books in every store
-        books = list(Book.objects.all())
-        for i in range(10):
-            temp_books = [books.pop(0) for _ in range(10)]
+            for publisher in Publisher.objects.all():
+                for _ in range(int(num)):
+                    books = [Book(
+                        name=fake.sentence(),
+                        price=random.uniform(29.99, 225.9),
+                        pages=random.randint(50, 300),
+                        rating=round(random.uniform(1, 5), 2),
+                        pubdate=timezone.now(),
+                        publisher=publisher)]
+                    Book.objects.bulk_create(books)
+
+        objs = Book.objects.all()
+        for _ in range(int(num)):
             store = Store.objects.create(name=fake.company())
-            store.books.set(temp_books)
+            store.books.set(objs)
             store.save()
-        self.stdout.write(self.style.SUCCESS('OK'))
